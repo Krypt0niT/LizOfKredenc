0816320aaaf21eac6f182a37d1d4be3edf7e9da7
Krypt0niT <k269692@gmail.com>
2022-04-14 12:49:56 +0200

    :)

--- LizOfKredenc/Assets/scripts/healthbar.cs
+++ LizOfKredenc/Assets/scripts/healthbar.cs
@@ -5,28 +5,26 @@ using UnityEngine;
 public class healthbar : MonoBehaviour
 {
     Transform t;
-    Quaternion startRotation;
     [SerializeField]
     GameObject playerTransform;
     [SerializeField]
-    GameObject Manager;
-
-    manager ManagerVariables;
-    int player1_health;
+    GameObject manager;
+    Manazer variables;
+    
     
 
     void Start()
     {
         t = GetComponent<Transform>();
-        startRotation = t.rotation;
         playerTransform.GetComponent<Transform>();
-        ManagerVariables = GetComponent<manager>();
-        player1_health = ManagerVariables.player1_health;   
-        print(player1_health);
+        variables = manager.GetComponent<Manazer>();
     }
 
     void Update()
     {
+        
+        print(variables.player1_health);
+        
         t.position = new Vector3(playerTransform.transform.position.x,transform.position.y,playerTransform.transform.position.z);
         if (transform.parent.name == "P1_healthBar")
         {
