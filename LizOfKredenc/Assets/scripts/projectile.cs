0c346901067dd2343344788381f0193b2321e68f
Krypt0niT <k269692@gmail.com>
2022-04-19 15:37:42 +0200

    19.4. PC

--- LizOfKredenc/Assets/scripts/projectile.cs
+++ LizOfKredenc/Assets/scripts/projectile.cs
@@ -6,7 +6,7 @@ public class projectile : MonoBehaviour
 {
     float timeAlive = 0;
     [SerializeField]
-    float DlzkaZivota = 5;
+    float DlzkaZivota = 5 ;
     float projectileSpeed = 10;
 
 
